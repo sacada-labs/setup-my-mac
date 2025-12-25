7ca24be6f3e85b175545f06c46128747bec73b6d
Guilherme <guilhermeoliveira564@gmail.com>
2025-12-25 18:00:55 +0100

    Add download progress indicator to install script

--- run.sh
+++ run.sh
@@ -8,17 +8,14 @@ BINARY="setup"
 TAG="latest"
 
 # Download binary to temporary file in /tmp
-echo "Downloading ${BINARY}..."
 TEMP_BINARY=$(mktemp /tmp/${BINARY}.XXXXXX)
-curl -sL "https://github.com/${REPO}/releases/download/${TAG}/${BINARY}" -o "${TEMP_BINARY}"
+curl -#L "https://github.com/${REPO}/releases/download/${TAG}/${BINARY}" -o "${TEMP_BINARY}"
 
 # Make executable
 chmod +x "${TEMP_BINARY}"
 
 # Move to final location
 mv "${TEMP_BINARY}" "${BINARY}"
-
-echo "Downloaded ${BINARY}"
 
 # Execute the binary
 ./${BINARY}
